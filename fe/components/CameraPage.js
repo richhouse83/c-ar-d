3f65b0ba7742d03e28394b7470bb39c1a13d82ae
Rich House <rich@richhouse.co.uk>
2021-02-23 17:04:13 +0000

    upload to aws functioning

--- fe/components/CameraPage.js
+++ fe/components/CameraPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import { RNS3 } from 'react-native-upload-aws-s3';
+import { v4 as uuidv4 } from 'uuid';
 
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
@@ -52,6 +54,44 @@ export default class CameraPage extends React.Component {
       </>
     );
   }
+  async uploadToS3(video) {
+    console.log('attempting to upload...');
+    const fileName = `${uuidv4()}.mov`;
+    const file = {
+      uri: video.uri,
+      name: fileName,
+      type: 'video/mov',
+    };
+    const options = {
+      keyPrefix: 'uploads/',
+      bucket: 'c-ar-d-video-storage',
+      region: 'us-east-2',
+      accessKey: 'AKIAZYOOKGI7R2JJIOXM',
+      secretKey: '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',
+      successActionStatus: 201,
+    };
+    try {
+      const response = await RNS3.put(file, options);
+      if (response.status === 201) {
+        console.log('Success: ', response.body);
+        /**
+         * {
+         *   postResponse: {
+         *     bucket: "your-bucket",
+         *     etag : "9f620878e06d28774406017480a59fd4",
+         *     key: "uploads/image.png",
+         *     location: "https://your-bucket.s3.amazonaws.com/uploads%2Fimage.png"
+         *   }
+         * }
+         */
+      } else {
+        console.log('Failed to upload image to S3: ', response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   handleRecording = async () => {
     const { recording } = this.state;
     let video = {};
@@ -63,6 +103,7 @@ export default class CameraPage extends React.Component {
       console.log(video.uri);
       this.setState({ recording: false });
       MediaLibrary.saveToLibraryAsync(video.uri);
+      this.uploadToS3(video);
     } else {
       this.setState({ recording: false });
       this.camera.stopRecording();
